68d8116670e3f53542d1813a78f5cae3c34effba
Alma Herrström <almaherrstrom@Almas-Air-4.lan>
2024-02-06 14:31:26 +0100

    did exercise 1-7 and started 8

--- src/index.js
+++ src/index.js
@@ -2,9 +2,9 @@
 1) (This has a DEMO solution.)
 Create a variable that stores the name of your cafe.
 Print out a greeting such as "Welcome to Technigo Cafe! What would you like to order today?"
-Where echnigo Cafe is replaced by the name of your cafe that is stored in the variable.
+Where Technigo Cafe is replaced by the name of your cafe that is stored in the variable.
 */
-const cafeName = "Technigo Cafe";
+const cafeName = "Supernova Cafe";
 console.log(`Welcome to ${cafeName}! What would you like to order today?`);
 
 /*
@@ -15,12 +15,23 @@ Print out the total price such as "There you go, that'll be 10 euros"
 Where 10 is replaced by the calculation of the total price.
 */
 
+let cafePrice = (46)
+let numberOfCafe = (5)
+
+console.log (`There you go, that'll be ${cafePrice * numberOfCafe} euros`);
+
 /*
 3)
 Create a variable that stores a boolean.
 Print out "You said this coffee is the best, that was actually true"
 Where true is replaced by your varible
 */
+
+let cafe = true
+let soda = false
+
+console.log (`You said this coffee is the best, that was actually ${cafe}`)
+ 
 
 /*
 4)
@@ -31,6 +42,10 @@ assign it a new value.
 Print it out. => This should give you the new value.
 */
 
+let cafeGuests = 52
+cafeGuests = 18
+console.log(cafeGuests)
+
 /*
 5)
 Create a variable called maxGuests, that shows us how many guests are allowed in the cafe.
@@ -40,16 +55,28 @@ assign it a new value.
 Print it out. => This should give you an error because it shouldn't be able to be changed.
 */
 
+const maxGuests = 80
+/*maxGuests = 100*/
+
+console.log(maxGuests)
+
 /*
 6)
 Create a variable that stores a string.
 Print out that string in only UPPERCASE letters.
 */
 
+let message = "we are learning javascript"
+console.log(message.toUpperCase())
+
 /*
 7)
 Print out the same string in only lowercase letters.
 */
+
+let string = "we are learning javascript"
+console.log(string.toLowerCase())
+
 
 /*
 8) **BONUS**
@@ -58,3 +85,8 @@ Then figure out a way to replace the word "summer" in the string with the word "
 Should give you => "Today we have a special winter deal!"
 (check for a specific string method...)
 */
+
+
+let station = "summer"
+let Newstation = station.replace("summer","winter")
+console.log(`Today we have a special ${station} deal!`);
